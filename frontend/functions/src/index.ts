71b4480cd295a3c198d7bf989b23191f8f743048
Minh Nguyen <minh.nguyen@hdwebsoft.co>
2020-10-22 15:02:51 +0700

    FE-03 attempt to deploy ssr with firebase

--- frontend/functions/src/index.ts
+++ frontend/functions/src/index.ts
@@ -1,9 +1,16 @@
-import * as functions from 'firebase-functions';
+const functions = require("firebase-functions");
+const { Nuxt } = require("nuxt-start");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// const nuxtConfig = require("../nuxt.config.js").default();
+
+const config = {
+//   ...nuxtConfig,
+  dev: false,
+  buildDir: "nuxt"
+};
+const nuxt = new Nuxt(config);
+
+exports.demoApp = functions.https.onRequest(async (req: any, res: any) => {
+  await nuxt.ready();
+  nuxt.render(req, res);
+});
